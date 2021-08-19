88f960b9101ade401fea3126137b61b0cb4f7eec
Mabelys Arias <MabelysA@gmail.com>
2021-08-19 16:33:44 -0400

    adds mostrequested park logic into parksindex page

--- src/views/ParksIndex.vue
+++ src/views/ParksIndex.vue
@@ -4,6 +4,10 @@
     View By Borough:
     <input v-model="boroughFilter" />
     <div v-for="(park, index) in filterBy(parks, boroughFilter, 'borough')" :key="park.id">
+      <button v-on:click="mostRequested()">Most Requested</button>
+      {{ mostRequestedPark }}
+      <br />
+
       <h2>{{ park.name }} - {{ index + 1 }}</h2>
       <p>
         <b>Borough:</b>
@@ -38,18 +42,50 @@ export default {
       message: "NYC Parks!",
       parks: [],
       boroughFilter: "",
+      committeds: [],
+      mostRequestedPark: "",
     };
   },
   mixins: [Vue2Filters.mixin],
   created: function () {
     this.indexParks();
+    this.indexCommitteds();
+    this.mostRequested();
+    setTimeout(this.mostRequested(), 1);
   },
+  // mounted: function () {
+  // },
   methods: {
     indexParks: function () {
       axios.get("http://localhost:3000/parks").then((response) => {
         this.parks = response.data;
         console.log("All parks:", this.parks);
       });
+    },
+    indexCommitteds: function () {
+      axios.get("http://localhost:3000/committeds/all").then((response) => {
+        this.committeds = response.data;
+        console.log("All committeds:", this.committeds);
+      });
+    },
+
+    mostRequested: function () {
+      console.log("displaying most requested park");
+      var newCommitteds = this.committeds.map(function (committed) {
+        return committed["park"]["name"];
+      });
+      console.log(newCommitteds);
+
+      var sortedCommitteds = newCommitteds.sort();
+      var start = newCommitteds[0],
+        mostRequestedPark;
+      for (var i = 0; i < sortedCommitteds.length; i++) {
+        if (start === sortedCommitteds[i] || sortedCommitteds[i] === sortedCommitteds[i + 1]) {
+          mostRequestedPark = sortedCommitteds[i];
+        }
+      }
+      console.log(mostRequestedPark);
+      this.mostRequestedPark = mostRequestedPark;
     },
   },
 };
