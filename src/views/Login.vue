be0c9e06d466fde45105099fe0d027df9e15d3b3
Mabelys Arias <MabelysA@gmail.com>
2021-08-20 14:09:48 -0400

    reroutes login page to parks page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -37,7 +37,7 @@ export default {
           localStorage.setItem("jwt", response.data.jwt);
           localStorage.setItem("user_id", response.data.user_id);
           console.log(response.data);
-          this.$router.push("/");
+          this.$router.push("/parks");
         })
         .catch((error) => {
           console.log(error.response);
