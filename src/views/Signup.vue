6ab54e3bad9ed35631511a2ce00dca66dcb75abe
Mabelys Arias <MabelysA@gmail.com>
2021-08-25 00:39:27 -0400

    modifies signup page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,38 +1,83 @@
 <template>
   <div class="signup">
-    <form v-on:submit.prevent="submit()">
-      <h1>Signup</h1>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>First Name:</label>
-        <input type="text" v-model="newUserParams.first_name" />
+    <!-- ======= About Section ======= -->
+    <br />
+    <br />
+    <section id="about" class="section-bg">
+      <div class="container-fluid">
+        <div class="section-header">
+          <h3 class="section-title">Sign Up</h3>
+          <span class="section-divider"></span>
+          <p class="section-description">
+            First step: Create an account with us!
+            <br />
+            Input your information below.
+          </p>
+        </div>
+        <form v-on:submit.prevent="submit()">
+          <ul>
+            <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+          </ul>
+          <div>
+            <label>First Name:</label>
+            <input type="text" v-model="newUserParams.first_name" />
+          </div>
+          <div>
+            <label>Last Name:</label>
+            <input type="text" v-model="newUserParams.last_name" />
+          </div>
+          <div>
+            <label>Email Address:</label>
+            <input type="email" v-model="newUserParams.email_address" />
+          </div>
+          <div>
+            <label>Phone Number:</label>
+            <input type="text" v-model="newUserParams.phone_number" />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" v-model="newUserParams.password" />
+          </div>
+          <div>
+            <label>Password confirmation:</label>
+            <input type="password" v-model="newUserParams.password_confirmation" />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <div class="row">
+          <div class="col-lg-6 about-img" dat-aos-delay="100">
+            <img src="assets/img/about-img.jpg" alt="" />
+          </div>
+
+          <div class="col-lg-6 content" dat-aos-delay="100">
+            <h2>Get Excited!</h2>
+            <h3>To tackle some trash!</h3>
+            <p>#BinIt!</p>
+            <ul>
+              <li>
+                <i class="bi bi-check-circle"></i>
+                Create an account.
+              </li>
+              <li>
+                <i class="bi bi-check-circle"></i>
+                Login.
+              </li>
+              <li>
+                <i class="bi bi-check-circle"></i>
+                Choose a park and input your observations!
+              </li>
+            </ul>
+            <p>Account creation in progress...</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <label>Last Name:</label>
-        <input type="text" v-model="newUserParams.last_name" />
-      </div>
-      <div>
-        <label>Email Address:</label>
-        <input type="email" v-model="newUserParams.email_address" />
-      </div>
-      <div>
-        <label>Phone Number:</label>
-        <input type="text" v-model="newUserParams.phone_number" />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" v-model="newUserParams.password" />
-      </div>
-      <div>
-        <label>Password confirmation:</label>
-        <input type="password" v-model="newUserParams.password_confirmation" />
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
+    </section>
+    <!-- End About Section -->
   </div>
 </template>
+
+<style></style>
 
 <script>
 import axios from "axios";
