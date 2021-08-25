39640c315288dd462e7adb6a88a9012fbccb7357
Mabelys Arias <MabelysA@gmail.com>
2021-08-24 22:32:48 -0400

    updates homepage with theme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,9 @@
           <br />
           <br />
           <h2>{{ message1 }}</h2>
-          <h1>
-            <b>
-              <i>{{ message2 }}</i>
-            </b>
-          </h1>
+          <p>
+            <i>{{ message2 }}</i>
+          </p>
           <p>{{ message3 }}</p>
           <p>{{ message4 }}</p>
           <a href="/signup" class="btn-get-started scrollto">Sign Up Here</a>
@@ -32,6 +30,123 @@
         </div>
       </section>
       <!-- End Hero Section -->
+      <!-- ======= Call To Action Section ======= -->
+      <section id="call-to-action">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-9 text-center text-lg-start">
+              <h3 class="cta-title">What is Tidy Park?</h3>
+              <p class="cta-text">
+                Tidy Park is an app where people can band together to clean up NYC, one park at a time!
+              </p>
+            </div>
+            <div class="col-lg-3 cta-btn-container text-center">
+              <a class="cta-btn align-middle" href="/parks">NYC Parks</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End Call To Action Section -->
+      <!-- ======= Contact Section ======= -->
+      <section id="contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-4">
+              <div class="contact-about">
+                <h3>Tidy Park</h3>
+                <p><b>Want to reach us? Use the form to contact us!</b></p>
+                <p>
+                  Post your park cleanup on social media using the hashtag
+                  <b>#TidyPark</b>
+                  !
+                </p>
+                <div class="social-links">
+                  <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                  <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                  <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                  <!-- <a href="#" class="snapchat"><i class="bi bi-snapchat"></i></a> -->
+                  <!-- <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a> -->
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-md-4">
+              <div class="info">
+                <div>
+                  <i class="bi bi-geo-alt"></i>
+                  <p>
+                    717 Park Avenue
+                    <br />
+                    New York, NY 10065
+                  </p>
+                </div>
+
+                <div>
+                  <i class="bi bi-envelope"></i>
+                  <p>hello@tidypark.com</p>
+                </div>
+
+                <div>
+                  <i class="bi bi-phone"></i>
+                  <p>212-555-5757</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-5 col-md-8">
+              <div class="form">
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                  <div class="row">
+                    <div class="form-group col-lg-6">
+                      <input
+                        type="text"
+                        name="name"
+                        class="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                        data-rule="minlen:4"
+                        data-msg="Please enter at least 4 chars"
+                      />
+                    </div>
+                    <div class="form-group col-lg-6 mt-3 mt-lg-0">
+                      <input
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        id="email"
+                        placeholder="Your Email"
+                        data-rule="email"
+                        data-msg="Please enter a valid email"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group mt-3">
+                    <input
+                      type="text"
+                      class="form-control"
+                      name="subject"
+                      id="subject"
+                      placeholder="Subject"
+                      data-rule="minlen:4"
+                      data-msg="Please enter at least 8 chars of subject"
+                    />
+                  </div>
+                  <div class="form-group mt-3">
+                    <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                  </div>
+                  <div class="my-3">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your message has been sent. Thank you!</div>
+                  </div>
+                  <div class="text-center"><button type="submit" title="Send Message">Send Message</button></div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End Contact Section -->
       <!-- OLD HOME CODE
       <h1>{{ message1 }}</h1>
       <h2>
