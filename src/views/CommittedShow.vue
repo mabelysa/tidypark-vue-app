7926921e3416b22858f6f1e3c778be8e89974ef9
Mabelys Arias <MabelysA@gmail.com>
2021-09-08 01:38:55 -0400

    changes park ids in committed show page

--- src/views/CommittedShow.vue
+++ src/views/CommittedShow.vue
@@ -35,147 +35,219 @@
           alt="park_id.map"
         />
       </p>
-      <p v-if="2 == park.id">
+      <p v-if="27 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8699,40.816,13.79,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8699,40.816,13.79,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="3 == park.id">
+      <p v-if="28 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8902,40.8965,12.9,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8902,40.8965,12.9,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="4 == park.id">
+      <p v-if="29 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8878,40.8059,16.32,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8878,40.8059,16.32,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="5 == park.id">
+      <p v-if="30 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8847,40.8601,12.6,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8847,40.8601,12.6,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="6 == park.id">
+      <p v-if="31 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9662,40.7827,12.6,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9662,40.7827,12.6,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="7 == park.id">
+      <p v-if="32 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9279,40.7918,13.3,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9279,40.7918,13.3,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="8 == park.id">
+      <p v-if="33 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.946,40.8415,14.01,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.946,40.8415,14.01,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="9 == park.id">
+      <p v-if="34 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9249,40.8729,14.21,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9249,40.8729,14.21,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="10 == park.id">
+      <p v-if="35 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.015,40.7032,15.76,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.015,40.7032,15.76,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="11 == park.id">
+      <p v-if="36 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9475,40.7568,15.62,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9475,40.7568,15.62,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="12 == park.id">
+      <p v-if="37 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8441,40.7404,14.03,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8441,40.7404,14.03,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="13 == park.id">
+      <p v-if="38 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8532,40.7026,12.99,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.8532,40.7026,12.99,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="14 == park.id">
+      <p v-if="39 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.7782,40.7923,14.4,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.7782,40.7923,14.4,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="15 == park.id">
+      <p v-if="40 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9224,40.7785,15.04,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9224,40.7785,15.04,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="16 == park.id">
+      <p v-if="41 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9975,40.6971,15.02,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9975,40.6971,15.02,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="17 == park.id">
+      <p v-if="42 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9785,40.5836,12.65,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9785,40.5836,12.65,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="18 == park.id">
+      <p v-if="43 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0312,40.6387,15.17,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0312,40.6387,15.17,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="19 == park.id">
+      <p v-if="44 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0046,40.672,14.04,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0046,40.672,14.04,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="20 == park.id">
+      <p v-if="45 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9447,40.6586,16.1,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-73.9447,40.6586,16.1,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="21 == park.id">
+      <p v-if="46 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0763,40.6373,17.21,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0763,40.6373,17.21,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="22 == park.id">
+      <p v-if="47 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0777,40.5825,14.56,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.0777,40.5825,14.56,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="23 == park.id">
+      <p v-if="48 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.1902,40.5812,13.5,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.1902,40.5812,13.5,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="24 == park.id">
+      <p v-if="49 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.1566,40.598,14.08,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.1566,40.598,14.08,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
-      <p v-if="25 == park.id">
+      <p v-if="50 == park.id">
         <img
-          src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.1598,40.5666,13.6,0/483x423?access_token=process.env.VUE_APP_TOKEN"
+          :src="
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-74.1598,40.5666,13.6,0/483x423?access_token=` +
+            key.api
+          "
           alt="park_id.map"
         />
       </p>
